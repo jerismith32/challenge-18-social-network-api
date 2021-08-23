01079cafa3c3fa190e6bb61071afee1fed8b375e
jerismith32 <jerismith32@gmail.com>
2021-08-23 19:00:12 -0400

    udpated user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -8,3 +8,18 @@ const {
     updateUser,
     deleteUser
 } = require('../../controllers/user-controller');
+
+// /api/users
+router
+    .route('/')
+    .get(getAllUser)
+    .post(createPizza);
+
+// /api/pizzas/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
